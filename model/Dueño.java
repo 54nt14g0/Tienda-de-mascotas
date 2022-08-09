aa0b9df4dc08204e1b7fcccbbb8c4bb0fc7880df
54nt14g0 <104014633+54nt14g0@users.noreply.github.com>
2022-08-09 23:59:56 +0200

    Se crea lo básico
    5 clases:
    Dueño que hereda de Persona
    Mascota que hereda de Animal
    y TiendaMascotas que contiene a todos

--- model/Dueño.java
+++ model/Dueño.java
@@ -6,10 +6,12 @@ public class Dueño extends Persona{
 
 	
 	private ArrayList <Mascota> listaMascotas = new ArrayList <>(); 
+	private double presupuesto;
 	// constructor
 	
-	public Dueño(String nombre, int edad, String codigo) {
+	public Dueño(String nombre, int edad, String codigo,double presupuesto) {
 		super(nombre, edad, codigo);
+		this.presupuesto = presupuesto;
 	}
 	// getters y setters
 	public ArrayList<Mascota> getListaMascotas() {
@@ -19,5 +21,12 @@ public class Dueño extends Persona{
 	public void setListaMascotas(ArrayList<Mascota> listaMascotas) {
 		this.listaMascotas = listaMascotas;
 	}
+	public double getPresupuesto() {
+		return presupuesto;
+	}
+	public void setPresupuesto(double presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+	
 	
 }
--- model/TiendaMascotas.java
+++ model/TiendaMascotas.java
@@ -6,6 +6,43 @@ public class TiendaMascotas {
 	
 	private ArrayList <Dueño> listaDueños = new ArrayList <>();
 	private ArrayList <Mascota> listaMascotas = new ArrayList <>();
-	private ArrayList <Factura> listaFacturas = new ArrayList <>();
+	
+	private String nombre;
+	
+	// getters y setters
+	public ArrayList<Dueño> getListaDueños() {
+		return listaDueños;
+	}
+	public void setListaDueños(ArrayList<Dueño> listaDueños) {
+		this.listaDueños = listaDueños;
+	}
+	public ArrayList<Mascota> getListaMascotas() {
+		return listaMascotas;
+	}
+	public void setListaMascotas(ArrayList<Mascota> listaMascotas) {
+		this.listaMascotas = listaMascotas;
+	}
+	
+	
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	// constructor
+	public TiendaMascotas(ArrayList<Dueño> listaDueños, ArrayList<Mascota> listaMascotas,
+			 String nombre) {
+		super();
+		this.listaDueños = listaDueños;
+		this.listaMascotas = listaMascotas;
+		this.nombre = nombre;
+	}
+	
+	
+	
+	
+	
 	
 }
